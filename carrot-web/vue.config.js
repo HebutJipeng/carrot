f364a4ba0020d889dfaab85d63adac643ba59468
HebutJipeng <282602893@qq.com>
2019-10-27 12:15:45 +0800

    Mod: 添加vue project

---
+++ carrot-web/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devServer: {
+    host: "127.0.0.1",
+    port: 8082,
+    https: false,
+    hotOnly: false,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:7001/",
+        ws: false,
+        changeOrigin: true
+      }
+    }
+  }
+};
