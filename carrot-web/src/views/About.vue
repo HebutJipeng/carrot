baaf99bd59da01d9fbb3b7c1dfa918fba789b9af
HebutJipeng <282602893@qq.com>
2019-10-27 12:29:34 +0800

    Mod: vue 中ts

--- carrot-web/src/views/About.vue
+++ carrot-web/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ greet }}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class About extends Vue {
+  greet: string = "This is an about page";
+  mounted() {
+    console.log(123);
+  }
+}
+</script>
